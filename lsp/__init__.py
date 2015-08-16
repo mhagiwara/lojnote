4f8ee85af7ef4325fbdf6bbdbc696d6201754973
Masato Hagiwara <masato@duolingo.com>
2015-08-16 19:18:04 -0400

    Implement number_sumti_in_sentence().

--- lsp/__init__.py
+++ lsp/__init__.py
@@ -1,9 +1,42 @@
-class Sentence(object):
-    pass
 
-class Node(object):
-    pass
+def semantic_parse(parse):
+    for node in parse:
+        if node.name == 'sentence':
+            number_sumti_in_sentence(node)
+        else:
+            semantic_parse(node)
 
-def convert():
-    """Convert camxes-python parse tree to lsp parse tree"""
-    pass
+FA_TAGS = {
+    'FA': 1,
+    'FE': 2,
+    'FI': 3,
+    'FO': 4,
+    'FU': 5
+}
+
+
+def number_sumti_in_sentence(parse):
+    """Number sumtis in a sentence"""
+
+    assert parse.name == 'sentence'
+
+    sentence_elements = []
+    for node in parse:
+        if node.name == 'bridiTail3':
+            for subnode in node:
+                if subnode.name == 'terms':
+                    for subsubnode in subnode:
+                        sentence_elements.append(subsubnode)
+                else:
+                    sentence_elements.append(subnode)
+        else:
+            sentence_elements.append(node)
+
+    sumti_counter = 1
+    print sentence_elements
+
+
+if __name__ == '__main__':
+    import camxes
+    parse = camxes.parse('mi klama fi le zarci bai lo broda')
+    print semantic_parse(parse)
