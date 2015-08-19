37076e850fa02a5c44522abb07708435a1b0207c
Masato Hagiwara <masato@duolingo.com>
2015-08-18 22:30:55 -0400

    Implement 'next_available_place' in place structure filling.

--- lsp/__init__.py
+++ lsp/__init__.py
@@ -1,7 +1,7 @@
 
 def semantic_parse(parse):
     for node in parse:
-        if node.name == 'sentence':
+        if node.name in ['sentence', 'bridiTail3']:
             number_sumti_in_sentence(node)
         else:
             semantic_parse(node)
@@ -15,10 +15,17 @@ FA_TAGS = {
 }
 
 
+def next_available_place(places, current_place):
+    current_place += 1
+    while current_place in places:
+        current_place += 1
+    return current_place
+
+
 def number_sumti_in_sentence(parse):
     """Number sumti in a sentence"""
 
-    assert parse.name == 'sentence'
+    assert parse.name in ['sentence', 'bridiTail3']
 
     sentence_elements = []
     for node in parse:
@@ -37,28 +44,30 @@ def number_sumti_in_sentence(parse):
     selbri_cands = [node for node in sentence_elements if node.name in ['selbri3', 'BRIVLA']]
     selbri_node = selbri_cands[0] if len(selbri_cands) > 0 else None
 
-    # now walk through sentence_elements
-    sumti_counter = 1
+    # now walk through sentence_elements and fill places
+    places = dict()     # a dict from place to sumti
+    current_place = 0
     for node in sentence_elements:
         print node
         if node.name == 'sumti6':
-            node.place = (selbri_node, sumti_counter)
-            print "sumti6 - %s (%s, %s)" % (node.lojban, selbri_node.lojban, sumti_counter)
-            # TODO: replace this with 'next unoccupied place'
-            sumti_counter += 1
+            current_place = next_available_place(places, current_place)
+            places[current_place] = node
+            print "sumti6 - %s (%s, %s)" % (node.lojban, selbri_node.lojban, current_place)
         elif node.name == 'term1':
             if node[0].name == 'CMAVO' and node[0][0].name == 'FA':
-                sumti_counter = FA_TAGS[node[0][0][0]]
-                node[1].place = (selbri_node, sumti_counter)
-                print "term1 - %s (%s, %s)" % (node[1].lojban, selbri_node.lojban, sumti_counter)
-                # TODO: replace this with 'next unoccupied place'
-                sumti_counter += 1
-            else:
-                pass
+                current_place = FA_TAGS[node[0][0][0]]
+                places[current_place] = node[1]
+                print "term1 - %s (%s, %s)" % (node[1].lojban, selbri_node.lojban, current_place)
 
-        elif node.name in ['selbri3', 'BRIVLA'] and sumti_counter == 1:
-            # TODO: replace this with 'next unoccupied place'
-            sumti_counter += 1
+        elif node.name in ['selbri3', 'BRIVLA'] and current_place == 0:
+            current_place = next_available_place(places, current_place)
         elif node.name == 'CMAVO':
-            node.place = (selbri_node, sumti_counter)
-            print "CMAVO - %s (%s, %s)" % (node.lojban, selbri_node.lojban, sumti_counter)
+            current_place = next_available_place(places, current_place)
+            places[current_place] = node
+            print "CMAVO - %s (%s, %s)" % (node.lojban, selbri_node.lojban, current_place)
+
+    # TODO - deal with SE here
+
+    # assign place infomation to each sumti/CMAVO
+    for place, node in places.iteritems():
+        node.place = (selbri_node, place)
--- lsp/test_lsp.py
+++ lsp/test_lsp.py
@@ -5,15 +5,28 @@ import lsp
 class TestLojbanSemanticParser(unittest.TestCase):
 
     def testNumberSumtiInSentence(self):
-        parse = camxes.parse('mi klama fi la .atlantas. le dargu fe la bastn. le karce')
-        print parse
+        # test case - without x1
+        parse = camxes.parse("broda zo'e")
+        lsp.semantic_parse(parse)
+        selbri = parse.bridiTail3[0].BRIVLA[0]
+        self.assertEqual((selbri, 2), parse.bridiTail3[0].CMAVO[0].place)
+
+        # test case - standard x1 CMAVO and x2 sumti
+        parse = camxes.parse("mi klama le zarci")
         lsp.semantic_parse(parse)
         selbri = parse.sentence[0].bridiTail3[0].BRIVLA[0]
         self.assertEqual((selbri, 1), parse.sentence[0].CMAVO[0].place)
+        self.assertEqual((selbri, 2), parse.sentence[0].bridiTail3[0].sumti6[0].place)
+
+        # test case - changing place by FA and filling subsequent places
+        parse = camxes.parse("mi klama fi la .atlantas. le dargu fe la bastn. le karce")
+        lsp.semantic_parse(parse)
+        selbri = parse.sentence[0].bridiTail3[0].BRIVLA[0]
+        self.assertEqual((selbri, 1), parse.sentence[0].CMAVO[0].place)
+        self.assertEqual((selbri, 3), parse.sentence[0].bridiTail3[0].terms[0].term1[0].sumti6[0].place)
+        self.assertEqual((selbri, 4), parse.sentence[0].bridiTail3[0].terms[0].sumti6[0].place)
         self.assertEqual((selbri, 2), parse.sentence[0].bridiTail3[0].terms[0].term1[1].sumti6[0].place)
-        self.assertEqual((selbri, 3), parse.sentence[0].bridiTail3[0].terms[0].sumti6[1].place)
-        self.assertEqual((selbri, 4), parse.sentence[0].bridiTail3[0].terms[0].term1[0].sumti6[0].place)
-        self.assertEqual((selbri, 5), parse.sentence[0].bridiTail3[0].terms[0].sumti6[0].place)
+        self.assertEqual((selbri, 5), parse.sentence[0].bridiTail3[0].terms[0].sumti6[1].place)
 
 if __name__ == '__main__':
     unittest.main()
