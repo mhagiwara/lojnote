05526176b648f533bd0ccd87ff8c0f3ccf1019eb
Masato Hagiwara <masato@duolingo.com>
2015-08-20 22:42:59 -0400

    Implement feature-value structure semantic parsing.

--- lsp/__init__.py
+++ lsp/__init__.py
@@ -1,8 +1,8 @@
 
 def semantic_parse(parse):
     for node in parse:
-        if node.name in ['sentence', 'bridiTail3']:
-            number_sumti_in_sentence(node)
+        if node.name in ['sentence', 'bridiTail3', 'BRIVLA']:
+            return parse_bridi(node)
         else:
             semantic_parse(node)
 
@@ -22,52 +22,70 @@ def next_available_place(places, current_place):
     return current_place
 
 
-def number_sumti_in_sentence(parse):
-    """Number sumti in a sentence"""
+def parse_bridi(parse):
+    """Parse a bridi into a feature-value structure.
 
-    assert parse.name in ['sentence', 'bridiTail3']
+       parse: a camxes parse tree
 
+       Returns a dict which corresponds to the feature-value structure of the bridi,
+       which contains the following keys:
+       1, 2, ...    : places
+       'selbri'     : main selbri of the bridi
+    """
+
+    assert parse.name in ['sentence', 'bridiTail3', 'BRIVLA']
+
+    print "Analyzing '%s'" % parse.lojban
+
+    # put sentence elements into an array
     sentence_elements = []
-    for node in parse:
-        if node.name == 'bridiTail3':
-            for subnode in node:
-                if subnode.name == 'terms':
-                    # put individual terms in sentence_elements
-                    for subsubnode in subnode:
-                        sentence_elements.append(subsubnode)
-                else:
-                    sentence_elements.append(subnode)
-        else:
-            sentence_elements.append(node)
+    if parse.name == 'BRIVLA':
+        sentence_elements.append(parse)
+    else:
+        for node in parse:
+            if node.name == 'bridiTail3':
+                for subnode in node:
+                    if subnode.name == 'terms':
+                        # put individual terms in sentence_elements
+                        for subsubnode in subnode:
+                            sentence_elements.append(subsubnode)
+                    else:
+                        sentence_elements.append(subnode)
+            else:
+                sentence_elements.append(node)
 
     # find the selbri element
     selbri_cands = [node for node in sentence_elements if node.name in ['selbri3', 'BRIVLA']]
     selbri_node = selbri_cands[0] if len(selbri_cands) > 0 else None
 
-    # now walk through sentence_elements and fill places
+    # now walk through sentence_elements and fill features
     places = dict()     # a dict from place to sumti
     current_place = 0
     for node in sentence_elements:
-        print node
         if node.name == 'sumti6':
             current_place = next_available_place(places, current_place)
             places[current_place] = node
-            print "sumti6 - %s (%s, %s)" % (node.lojban, selbri_node.lojban, current_place)
+            print "\tsumti6: %s -> (%s, %s)" % (node.lojban, selbri_node.lojban, current_place)
         elif node.name == 'term1':
             if node[0].name == 'CMAVO' and node[0][0].name == 'FA':
                 current_place = FA_TAGS[node[0][0][0]]
                 places[current_place] = node[1]
-                print "term1 - %s (%s, %s)" % (node[1].lojban, selbri_node.lojban, current_place)
+                print "\tterm1: %s -> (%s, %s)" % (node[1].lojban, selbri_node.lojban, current_place)
 
         elif node.name in ['selbri3', 'BRIVLA'] and current_place == 0:
             current_place = next_available_place(places, current_place)
-        elif node.name == 'CMAVO':
+
+        elif node.name == 'CMAVO' and node[0].name != 'CU':
             current_place = next_available_place(places, current_place)
             places[current_place] = node
-            print "CMAVO - %s (%s, %s)" % (node.lojban, selbri_node.lojban, current_place)
+            print "\tCMAVO: %s -> (%s, %s)" % (node.lojban, selbri_node.lojban, current_place)
+        else:
+            print "\t%s: %s - no place" % (node.name, node.lojban)
 
     # TODO - deal with SE here
 
-    # assign place infomation to each sumti/CMAVO
-    for place, node in places.iteritems():
-        node.place = (selbri_node, place)
+
+    # put selbri
+    places['selbri'] = selbri_node
+
+    return places
--- lsp/test_lsp.py
+++ lsp/test_lsp.py
@@ -5,28 +5,49 @@ import lsp
 class TestLojbanSemanticParser(unittest.TestCase):
 
     def testNumberSumtiInSentence(self):
+        # test case - bare selbri
+        parse = camxes.parse("broda")
+        sparse = lsp.semantic_parse(parse)
+        self.assertEqual(sparse['selbri'].lojban, "broda")
+
         # test case - without x1
         parse = camxes.parse("broda zo'e")
-        lsp.semantic_parse(parse)
-        selbri = parse.bridiTail3[0].BRIVLA[0]
-        self.assertEqual((selbri, 2), parse.bridiTail3[0].CMAVO[0].place)
+        sparse = lsp.semantic_parse(parse)
+        self.assertEqual(sparse['selbri'].lojban, "broda")
+        self.assertEqual(sparse[2].lojban, "zo'e")
+
+        # test case - standard x1 to x4
+        parse = camxes.parse("mi tavla do zo'e zo'e")
+        sparse = lsp.semantic_parse(parse)
+        self.assertEqual(sparse['selbri'].lojban, "tavla")
+        self.assertEqual([sparse[i].lojban for i in [1, 2, 3, 4]], ["mi", "do", "zo'e", "zo'e"])
 
         # test case - standard x1 CMAVO and x2 sumti
         parse = camxes.parse("mi klama le zarci")
-        lsp.semantic_parse(parse)
+        sparse = lsp.semantic_parse(parse)
         selbri = parse.sentence[0].bridiTail3[0].BRIVLA[0]
-        self.assertEqual((selbri, 1), parse.sentence[0].CMAVO[0].place)
-        self.assertEqual((selbri, 2), parse.sentence[0].bridiTail3[0].sumti6[0].place)
+        self.assertEqual(sparse['selbri'], selbri)
+        self.assertEqual(sparse[1], parse.sentence[0].CMAVO[0])
+        self.assertEqual(sparse[2], parse.sentence[0].bridiTail3[0].sumti6[0])
+
+        # test case - standard
+        parse = camxes.parse("mi cu klama le zarci")
+        sparse = lsp.semantic_parse(parse)
+        selbri = parse.sentence[0].bridiTail3[0].BRIVLA[0]
+        self.assertEqual(sparse['selbri'], selbri)
+        self.assertEqual(sparse[1], parse.sentence[0].CMAVO[0])
+        self.assertEqual(sparse[2], parse.sentence[0].bridiTail3[0].sumti6[0])
 
         # test case - changing place by FA and filling subsequent places
         parse = camxes.parse("mi klama fi la .atlantas. le dargu fe la bastn. le karce")
-        lsp.semantic_parse(parse)
+        sparse = lsp.semantic_parse(parse)
         selbri = parse.sentence[0].bridiTail3[0].BRIVLA[0]
-        self.assertEqual((selbri, 1), parse.sentence[0].CMAVO[0].place)
-        self.assertEqual((selbri, 3), parse.sentence[0].bridiTail3[0].terms[0].term1[0].sumti6[0].place)
-        self.assertEqual((selbri, 4), parse.sentence[0].bridiTail3[0].terms[0].sumti6[0].place)
-        self.assertEqual((selbri, 2), parse.sentence[0].bridiTail3[0].terms[0].term1[1].sumti6[0].place)
-        self.assertEqual((selbri, 5), parse.sentence[0].bridiTail3[0].terms[0].sumti6[1].place)
+        self.assertEqual(sparse['selbri'], selbri)
+        self.assertEqual(sparse[1], parse.sentence[0].CMAVO[0])
+        self.assertEqual(sparse[3], parse.sentence[0].bridiTail3[0].terms[0].term1[0].sumti6[0])
+        self.assertEqual(sparse[4], parse.sentence[0].bridiTail3[0].terms[0].sumti6[0])
+        self.assertEqual(sparse[2], parse.sentence[0].bridiTail3[0].terms[0].term1[1].sumti6[0])
+        self.assertEqual(sparse[5], parse.sentence[0].bridiTail3[0].terms[0].sumti6[1])
 
 if __name__ == '__main__':
     unittest.main()
