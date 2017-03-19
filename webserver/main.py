8258ade25d92870c4cee9cfc07299919c7694627
Masato Hagiwara <masato@duolingo.com>
2017-03-18 21:21:00 -0400

    Switch framework to flask.

--- webserver/main.py
+++ webserver/main.py
@@ -1,43 +1,17 @@
-import os
-
-import pystache
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-from pyramid.asset import resolve_asset_spec
-from pyramid.view import view_config
-from pyramid.path import package_path
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-class MustacheRendererFactory(object):
-    def __init__(self, info):
-        self.info = info
-
-    def __call__(self, value, system):
-        package, filename = resolve_asset_spec(self.info.name)
-        template = os.path.join(package_path(self.info.package), filename)
-        template_fh = open(template)
-        template_stream = template_fh.read()
-        template_fh.close()
-        return pystache.render(template_stream, value)
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 
-@view_config(route_name='hello')
-def hello_world(request):
-    return Response('Hello %(name)s!' % request.matchdict)
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return render_template('hello_world.html', name=username)
 
 
-@view_config(route_name='hello2', renderer='hello_world.tmpl')
-def hello_world2(request):
-    return dict(name=request.matchdict['name'])
-
-
-if __name__ == '__main__':
-    config = Configurator()
-    config.add_renderer(name='.tmpl', factory='webserver.main.MustacheRendererFactory')
-    config.add_route('hello', '/hello/{name}')
-    config.add_route('hello2', '/hello2/{name}')
-    config.scan()
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 8180, app)
-    server.serve_forever()
+if __name__ == "__main__":
+    app.run()
