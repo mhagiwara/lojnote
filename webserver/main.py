8eacebf94d94fdca26922b6dabc05fbc492b9edb
Masato Hagiwara <masato@duolingo.com>
2017-04-12 18:10:18 -0400

    Start implementing LDP standalone parse route.

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from ldp.depparser import parse as ldp_parse
+
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 
@@ -11,6 +13,18 @@ def hello():
 def show_user_profile(username):
     # show the user profile for that user
     return render_template('hello_world.html', name=username)
+
+
+@app.route('/parse/<sentence>')
+def parse(sentence):
+    """Show a parse tree for the given sentence using LDP."""
+    parse_str = str(list(ldp_parse(sentence)))
+    return render_template('parse.html', sentence=sentence, parse_str=parse_str)
+
+
+@app.route('/static/<path:path>')
+def serve_static_files(path):
+    return send_from_directory('static', path)
 
 
 if __name__ == "__main__":
