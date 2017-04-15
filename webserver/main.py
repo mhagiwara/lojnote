6c5559984873a8e28baf4b4752160630f897c13e
Masato Hagiwara <masato@duolingo.com>
2017-04-14 22:00:47 -0400

    Replace mock with real input.

--- webserver/main.py
+++ webserver/main.py
@@ -18,8 +18,28 @@ def show_user_profile(username):
 @app.route('/parse/<sentence>')
 def parse(sentence):
     """Show a parse tree for the given sentence using LDP."""
-    parse_str = str(list(ldp_parse(sentence)))
-    return render_template('parse.html', sentence=sentence, parse_str=parse_str)
+    tokens = next(ldp_parse(sentence))
+    nodes = []
+    edges = []
+    for i, token in enumerate(tokens):
+        nodes.append({
+          'data': {'id': 'n%d' % (i + 1), 'title': token.form},
+          'position': {'x': i * 100, 'y': 0}
+        })
+
+        edge = {'data': {'id': str(i),
+                         'source': 'n%d' % (i + 1),
+                         'target': 'n%d' % (token.head),
+                         'direction': 1}}
+        inversed = token.head < i + 1
+        if inversed:
+            edge['style'] = {'control-point-distances': '50 50'}
+        edges.append(edge)
+
+    return render_template('parse.html',
+                           sentence=sentence,
+                           nodes=nodes,
+                           edges=edges)
 
 
 @app.route('/static/<path:path>')
